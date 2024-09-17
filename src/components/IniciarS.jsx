69ef4760de4a85d85db40ef42ba3b73f7f9f3adc
RobertoAPR <robertorios@MBP-de-Roberto.bwrouter>
2024-09-17 16:23:03 -0600

    Parte front con login terminada ahora trabajaremos backend este sera el punto de control en este cambio

--- src/components/IniciarS.jsx
+++ src/components/IniciarS.jsx
@@ -4,80 +4,99 @@ import { FaEye } from "react-icons/fa";
 import React, {useState} from "react"
 import { FaEyeSlash } from "react-icons/fa";
 import {Link, Outlet} from "react-router-dom";
+import usua from '../assets/usu.png'
 
 export default function IniciarS() {
   const [showPassword,setShowPassword] = useState(false)
+  const [data,setData] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleOnChange = (e) => {
+    const {name, value} = e.target
+
+    setData((preve)=>{
+      return {
+        ...preve,
+        [name]: value
+    }
+  })
+  }
+
+  const handleSubmit = (e) => {
+   e.preventDefault()
+    // API call to send data
+    
+  }
+
+  console.log("data login",data)
 
   return (
     <>
+    <section id='login'>
     <Navbar/>
-     <div className="max-w-7xl mx-auto pt-20 px-6">
-    
-    <div >
-      <div className=" w-full items-center justify-center ">
-      <div className='bg-slate-800 px-10 py-20 rounded-3xl border-2 border-gray-100'>
-          <h1 className='text-5xl font-semibold'>Welcome Back</h1>
-          <p className='font-medium text-lg text-gray-200 mt-4'>Welcome back! please enter your details.</p>
-          <div className='mt-8'>
-            <div>
-              <label className='text-lg font-medium'>Email</label>
+     <div className="mx-auto container p-4">
+      <div className='bg-slate-800 p-5 w-full max-w-sm mx-auto rounded-3xl border-2 border-gray-100'>
+        <div className='w-20 h-20 mx-auto'>
+          <img src={usua} alt="login icons" />
+        </div>
+
+        <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
+          <div className='grid'>
+              <label>Email</label>
+              <div className='ease-in-out transition-all flex  border-2 border-gray-100 items-center justify-center py-3 rounded-xl'>
               <input 
-              type='email' className='w-full h-full outline-none  bg-transparent'
-              placeholder='Enter your email'
-              />
-            </div>
-            <div>
-              <label className='text-lg font-medium'>Password</label>
-              <div className='p-2 flex'>
+               type='email' 
+               className='w-full h-full outline-none  bg-transparent'
+               name='email'
+               value={data.email}
+               onChange={handleOnChange}
+               placeholder='Enter your email'
+               />
+              </div>
+          </div>
+
+          <div>
+              <label>Password:</label>
+              <div className=' ease-in-out transition-all flex  border-2 border-gray-100 items-center justify-center py-3 rounded-xl'>
               <input 
-              type={showPassword ? "text":"password"} className='w-full h-full outline-none  bg-transparent'
-              placeholder='Enter your Password'
+             type={showPassword ? "text":"password"} 
+             className='w-full h-full outline-none  bg-transparent'
+             value={data.password}
+             name='password'
+             onChange={handleOnChange}
+             placeholder='Enter your Password'
               />
+              
               <div className='cursor-pointer text-xl' onClick={()=>setShowPassword((preve)=>!preve)}>
                 <span>
                   {
                     showPassword? (
                       <FaEyeSlash/>
                     ) : (
+                      
                       <FaEye/>
                     )
                   }
                
                 </span>
               </div>
-              </div>
             </div>
-            <div className='mt-8 flex justify-between items-center'>
-              <div>
-                <input
-                type='checkbox'
-                id='remember'
-                />
-                <label className='ml-2 font-medium text-base' for="remember">Remember for 30 days</label>
-              </div>
-              <button className='font-medium text-base text-orange-500 hover:underline hover:text-white'>
-                <Link to={'/forgot-password'}>
-                Forgot password
-                </Link>
-                </button>
-            </div>
+            <Link to={'/forgot-password'} className='block w-fit ml-auto hover:underline hover:text-orange-600 mt-2'>
+                  Forgot Password ?
+            </Link>
           </div>
-          <div className='mt-8 flex flex-col gap-y-4'>
-            <button className='active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all bg-gradient-to-r from-orange-500 to-orange-800 py-3 rounded-xl'>Sign in</button>
-            <button className='flex rounded-xl border-2 border-gray-100 items-center justify-center gap-2 active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all'>
-            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 48 48">
-            <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
-            </svg>
-              Sign in with Google
-            </button>
-          </div>
-          <p className='my-5'>Dont have account ? <Link to={"/sign-up"} className='text-orange-600 text-orange-700 hover:underline'>Sing up</Link></p>
+          
+          <button className='bg-gradient-to-r from-orange-500 to-orange-800 px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Sign up</button>
+
+        </form>
+        <p className='my-5'>Don't have account ? <Link to={"/sign-up"} className='text-orange-600 hover:text-orange-700 hover:underline'>Sign up</Link></p>
       </div>
-      </div>
-   
+    
     </div>
     <Footer/>
-    </div>
+    </section>
     </>
   )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,12 +1,177 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Store/Header'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { FaEye } from 'react-icons/fa'
+import { FaEyeSlash } from "react-icons/fa"
+import { Link } from 'react-router-dom'
+import usua from '../assets/usu.png'
+import imageTobasee from '../helpers/imageTobasee'
 
 const SignUp = () => {
+  const [showPassword,setShowPassword] = useState(false)
+  const [showConfirmPassword,setShowConfirmPassword] = useState(false)
+  const [data,setData] = useState({
+    email: "",
+    password: "",
+    name: "",
+    confirmPassword: "",
+    profilePic: "",
+
+  })
+
+  const handleOnChange = (e) => {
+    const {name, value} = e.target
+
+    setData((preve)=>{
+      return {
+        ...preve,
+        [name]: value
+    }
+  })
+  }
+
+  const handleUploadPic = async(e) => {
+      const file = e.target.files[0]
+
+      const imagePic = await imageTobasee(file)
+      console.log("imagePic",imagePic)
+      setData((preve)=>{
+          return{
+             ...preve,
+              profilePic: imagePic
+          }
+      })
+  }
+
+  const handleSubmit = (e) => {
+   e.preventDefault()
+    // API call to send data
+    
+  }
+
+  console.log("data login",data)
   return (
-    <div>
-        <Header/>
-      inicia sesion o regs¿istrate
+    <>
+    <p>esto es regustro de seccion</p>
+
+     <section id='signup'>
+    <Navbar/>
+     <div className="mx-auto container p-4">
+      <div className='bg-slate-800 p-5 w-full max-w-sm mx-auto rounded-3xl border-2 border-gray-100'>
+        
+        <div className='w-20 h-20 mx-auto relative overflow-hidden rounded-full'>
+          <div>
+            <img src={data.profilePic || usua} alt="login icons" />
+          </div>
+          <form >
+            <label>
+              <div className='text-xs bg-opacity-80 bg-slate-200 pb-4 pt-2 cursor-pointer text-center absolute bottom-0 w-full'>
+              Photo
+              </div>
+              <input type="file" className='hidden' onChange={handleUploadPic}/>
+            </label>
+          </form>
+        </div>
+
+        <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
+          <div className='name'>
+                <label>Name</label>
+                <div className='ease-in-out transition-all flex  border-2 border-gray-100 items-center justify-center py-3 rounded-xl'>
+                <input 
+                type='text' 
+                className='w-full h-full outline-none  bg-transparent'
+                name='name'
+                value={data.name}
+                onChange={handleOnChange}
+                required
+                placeholder='Enter your name'
+                />
+                </div>
+            </div>
+          <div className='grid'>
+              <label>Email</label>
+              <div className='ease-in-out transition-all flex  border-2 border-gray-100 items-center justify-center py-3 rounded-xl'>
+              <input 
+               type='email' 
+               className='w-full h-full outline-none  bg-transparent'
+               name='email'
+               value={data.email}
+               onChange={handleOnChange}
+               required
+               placeholder='Enter your email'
+               />
+              </div>
+          </div>
+
+          <div>
+              <label>Password:</label>
+              <div className=' ease-in-out transition-all flex  border-2 border-gray-100 items-center justify-center py-3 rounded-xl'>
+              <input 
+             type={showPassword ? "text":"password"} 
+             className='w-full h-full outline-none  bg-transparent'
+             value={data.password}
+             name='password'
+             onChange={handleOnChange}
+             required
+             placeholder='Enter your Password'
+              />
+              
+              <div className='cursor-pointer text-xl' onClick={()=>setShowPassword((preve)=>!preve)}>
+                <span>
+                  {
+                    showPassword? (
+                      <FaEyeSlash/>
+                    ) : (
+                      
+                      <FaEye/>
+                    )
+                  }
+               
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div>
+              <label>Confirm Password:</label>
+              <div className=' ease-in-out transition-all flex  border-2 border-gray-100 items-center justify-center py-3 rounded-xl'>
+              <input 
+             type={showConfirmPassword ? "text":"password"} 
+             className='w-full h-full outline-none  bg-transparent'
+             value={data.confirmPassword}
+             name='password'
+             onChange={handleOnChange}
+             required
+             placeholder='Enter confirm password'
+              />
+              
+              <div className='cursor-pointer text-xl' onClick={()=>setShowConfirmPassword((preve)=>!preve)}>
+                <span>
+                  {
+                    showConfirmPassword? (
+                      <FaEyeSlash/>
+                    ) : (
+                      
+                      <FaEye/>
+                    )
+                  }
+               
+                </span>
+              </div>
+            </div>
+          </div>
+          
+          <button className='bg-gradient-to-r from-orange-500 to-orange-800 px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Sign Up</button>
+
+        </form>
+        <p className='my-5'>Already have account ? <Link to={"/log"} className='text-orange-600 hover:text-orange-700 hover:underline'>Sign in</Link></p>
+      </div>
+    
     </div>
+    <Footer/>
+    </section>
+    </>
   )
 }
 
